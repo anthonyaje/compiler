b7f88aac07cd253d49b83ea99363ffc19c9d2fb3
Anthony Ang <anthonyang86@gmail.com>
2014-05-12 22:32:54 +0800

    hw4 added

---
+++ hw4/2014HW4/semanticAnalysis.c
@@ -0,0 +1,337 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "header.h"
+#include "symbolTable.h"
+// This file is for reference only, you are not required to follow the implementation. //
+// You only need to check for errors stated in the hw4 document. //
+int g_anyErrorOccur = 0;
+
+DATA_TYPE getBiggerType(DATA_TYPE dataType1, DATA_TYPE dataType2);
+void processProgramNode(AST_NODE *programNode);
+void processDeclarationNode(AST_NODE* declarationNode);
+void declareIdList(AST_NODE* typeNode, SymbolAttributeKind isVariableOrTypeAttribute, int ignoreArrayFirstDimSize);
+void declareFunction(AST_NODE* returnTypeNode);
+void processDeclDimList(AST_NODE* variableDeclDimList, TypeDescriptor* typeDescriptor, int ignoreFirstDimSize);
+void processTypeNode(AST_NODE* typeNode);
+void processBlockNode(AST_NODE* blockNode);
+void processStmtNode(AST_NODE* stmtNode);
+void processGeneralNode(AST_NODE *node);
+void checkAssignOrExpr(AST_NODE* assignOrExprRelatedNode);
+void checkWhileStmt(AST_NODE* whileNode);
+void checkForStmt(AST_NODE* forNode);
+void checkAssignmentStmt(AST_NODE* assignmentNode);
+void checkIfStmt(AST_NODE* ifNode);
+void checkWriteFunction(AST_NODE* functionCallNode);
+void checkFunctionCall(AST_NODE* functionCallNode);
+void processExprRelatedNode(AST_NODE* exprRelatedNode);
+void checkParameterPassing(Parameter* formalParameter, AST_NODE* actualParameter);
+void checkReturnStmt(AST_NODE* returnNode);
+void processExprNode(AST_NODE* exprNode);
+void processVariableLValue(AST_NODE* idNode);
+void processVariableRValue(AST_NODE* idNode);
+void processConstValueNode(AST_NODE* constValueNode);
+void getExprOrConstValue(AST_NODE* exprOrConstNode, int* iValue, float* fValue);
+void evaluateExprValue(AST_NODE* exprNode);
+
+
+typedef enum ErrorMsgKind
+{
+    SYMBOL_IS_NOT_TYPE,
+    SYMBOL_REDECLARE,
+    SYMBOL_UNDECLARED,
+    NOT_FUNCTION_NAME,
+    TRY_TO_INIT_ARRAY,
+    EXCESSIVE_ARRAY_DIM_DECLARATION,
+    RETURN_ARRAY,
+    VOID_VARIABLE,
+    TYPEDEF_VOID_ARRAY,
+    PARAMETER_TYPE_UNMATCH,
+    
+TOO_FEW_ARGUMENTS,
+    TOO_MANY_ARGUMENTS,
+    RETURN_TYPE_UNMATCH,
+    INCOMPATIBLE_ARRAY_DIMENSION,
+    NOT_ASSIGNABLE,
+    NOT_ARRAY,
+    IS_TYPE_NOT_VARIABLE,
+    IS_FUNCTION_NOT_VARIABLE,
+    STRING_OPERATION,
+    ARRAY_SIZE_NOT_INT,
+    ARRAY_SIZE_NEGATIVE,
+    ARRAY_SUBSCRIPT_NOT_INT,
+    PASS_ARRAY_TO_SCALAR,
+    PASS_SCALAR_TO_ARRAY
+} ErrorMsgKind;
+
+void printErrorMsgSpecial(AST_NODE* node1, char* name2, ErrorMsgKind errorMsgKind)
+{
+    g_anyErrorOccur = 1;
+    printf("Error found in line %d\n", node1->linenumber);
+    /*
+    switch(errorMsgKind)
+    {
+    default:
+        printf("Unhandled case in void printErrorMsg(AST_NODE* node, ERROR_MSG_KIND* errorMsgKind)\n");
+        break;
+    }
+    */
+}
+
+
+void printErrorMsg(AST_NODE* node, ErrorMsgKind errorMsgKind)
+{
+    g_anyErrorOccur = 1;
+    printf("Error found in line %d\n", node->linenumber);
+    /*
+    switch(errorMsgKind)
+    {
+        printf("Unhandled case in void printErrorMsg(AST_NODE* node, ERROR_MSG_KIND* errorMsgKind)\n");
+        break;
+    }
+    */
+}
+
+
+void semanticAnalysis(AST_NODE *root)
+{
+    processProgramNode(root);
+}
+
+
+DATA_TYPE getBiggerType(DATA_TYPE dataType1, DATA_TYPE dataType2)
+{
+    if(dataType1 == FLOAT_TYPE || dataType2 == FLOAT_TYPE) {
+        return FLOAT_TYPE;
+    } else {
+        return INT_TYPE;
+    }
+}
+
+
+void processProgramNode(AST_NODE *programNode)
+{
+  AST_NODE* child = programNode->child;
+  switch(child){
+      case DECLARATION_NODE:
+	  processDeclarationNode(child);
+          break;
+      default:
+   	  printf("uncaught case\n"); 
+  }
+}
+
+void processDeclarationNode(AST_NODE* declarationNode)
+{
+  if(declarationNode == NULL){ return; }
+  else{
+     AST_NODE* child = declarationNode->child;
+     switch(declarationNode->semantic_value.declSemanticValue.kind){
+        case VARIABLE_DECL :
+  	    declareIdList(declarationNode, VARIABLE_ATTRIBUTE, 1);
+            break;
+
+        case TYPE_DECL :
+            break;
+
+        case FUNCTION_DECL:
+	    declareFunction(declarationNode);
+            break;
+
+        case FUNCTION_PARAMETER_DECL:
+	    
+            break;
+        default:
+   	    printf("uncaught case\n"); 
+   }
+   processDeclarationNode(declarationNode->rightSibling);
+ }
+
+
+
+}
+
+
+void processTypeNode(AST_NODE* idNodeAsType)
+{
+}
+
+void declareIdList(AST_NODE* declarationNode, SymbolAttributeKind isVariableOrTypeAttribute, int ignoreArrayFirstDimSize)
+{
+  AST_NODE* child = declarationNode->child;
+  if(isVariableOrTypeAttribute == VARIABLE_ATTRIBUTE){
+  	AST_NODE* p = child->rightSibling;
+  	while(p!=NULL){
+	    SymbolAttribute* symbol_att = (SymbolAttribute*) malloc(sizeof(SymbolAttribute));
+	    symbol_att->attributeKind = isVariableOrTypeAttribute;
+   	    TypeDescriptor* type_desc = (TypeDescriptor*) malloc(sizeof(TypeDescriptor));
+	    type_desc->kind = SCALAR_TYPE_DESCRIPTOR;
+	    type_desc->properties = p->dataType;
+	    symbol_att->attr = type_desc;
+  	    enterSymbol(p->semantic_value.identifierSemanticValue.identifierName,symbol_att);
+	    p = p->rightSibling;
+	}
+  } else{
+	//FIXME ignore
+  	AST_NODE* p = child;
+  	while(p!=NULL){
+	    SymbolAttribute* symbol_att = (SymbolAttribute*) malloc(sizeof(SymbolAttribute));
+	    symbol_att->attributeKind = isVariableOrTypeAttribute;
+   	    TypeDescriptor* type_desc = (TypeDescriptor*) malloc(sizeof(TypeDescriptor));
+	    type_desc->kind = SCALAR_TYPE_DESCRIPTOR;
+	    type_desc->properties = p->dataType;
+	    symbol_att->attr = type_desc;
+
+	    enterSymbol(p->semantic_value.identifierSemanticValue.identifierName,symbol_att);
+	    p = p->rightSibling;
+	}
+	
+  }
+}
+
+void checkAssignOrExpr(AST_NODE* assignOrExprRelatedNode)
+{
+	
+}
+
+void checkWhileStmt(AST_NODE* whileNode)
+{
+}
+
+
+void checkForStmt(AST_NODE* forNode)
+{
+}
+
+
+void checkAssignmentStmt(AST_NODE* assignmentNode)
+{
+}
+
+
+void checkIfStmt(AST_NODE* ifNode)
+{
+}
+
+void checkWriteFunction(AST_NODE* functionCallNode)
+{
+}
+
+void checkFunctionCall(AST_NODE* functionCallNode)
+{
+}
+
+void checkParameterPassing(Parameter* formalParameter, AST_NODE* actualParameter)
+{
+}
+
+
+void processExprRelatedNode(AST_NODE* exprRelatedNode)
+{
+}
+
+void getExprOrConstValue(AST_NODE* exprOrConstNode, int* iValue, float* fValue)
+{
+}
+
+void evaluateExprValue(AST_NODE* exprNode)
+{
+}
+
+
+void processExprNode(AST_NODE* exprNode)
+{
+}
+
+
+void processVariableLValue(AST_NODE* idNode)
+{
+}
+
+void processVariableRValue(AST_NODE* idNode)
+{
+}
+
+
+void processConstValueNode(AST_NODE* constValueNode)
+{
+}
+
+
+void checkReturnStmt(AST_NODE* returnNode)
+{
+}
+
+
+void processBlockNode(AST_NODE* blockNode)
+{
+}
+
+
+void processStmtNode(AST_NODE* stmtNode)
+{
+}
+
+
+void processGeneralNode(AST_NODE *node)
+{
+}
+
+void processDeclDimList(AST_NODE* idNode, TypeDescriptor* typeDescriptor, int ignoreFirstDimSize)
+{
+}
+
+
+void declareFunction(AST_NODE* declarationNode)
+{
+    AST_NODE* p = declarationNode->child;   
+    FunctionSignature* functionSig;
+    functionSig = (FunctionSignature*) malloc(sizeof(FunctionSignature));
+    functionSig->parameterList = NULL;
+    functionSig->parametersCount = 0;
+    functionSig->returnType = p->DATA_TYPE;
+    
+    p = p->rightSibling;
+    char* function_name = p->semantic_value.identifierSemanticValue.identifierName;
+
+    p = p->rightSibling;
+    AST_NODE* p_to_block = p;
+    
+    //p = p->child==NULL?p:p->child;
+    if(p->child != NULL){
+	p = p->child;
+	while(p!=NULL){
+           AST_NODE* q = p->child;
+	   functionSig->parametersCount++;
+           if(functionSig->parameterList == NULL){
+	       functionSig->parameterList = (Parameter*) malloc(sizeof(Parameter)); 	    
+	       functionSig->parameterList->next=NULL;
+	        	    
+   	       TypeDescriptor* type_desc = (TypeDescriptor*) malloc(sizeof(TypeDescriptor));
+	       type_desc->kind = q->rightSibling->semantic_value.identifierSemanticValue.kind==NORMAL_ID?SCALAR_TYPE_DESCRIPTOR:ARRAY_TYPE_DESCRIPTOR;
+	       if(type_desc->kind==SCALAR_TYPE_DESCRIPTOR){
+		//FIXME assume: dataType in the variable name
+ 		  type_desc->properties.dataType  = q->rightSibling->dataType;
+	       }else{
+		//TODO: traverse once to compute the dim
+		  type_desc->properties.arrayProperties.dimension;	//ASSIGN
+		  type_desc->properties.arrayProperties.sizeInEachDimension; 
+		  type_desc->properties.arrayProperties.elementType;
+	       }
+	       functionSig->parameterList->type=type_desc;
+	       functionSig->parameterList->parameterName = q->semantic_value.identifierSemanticValue.identifierName;	    
+ 	   }
+	   //TODO:IMPORTANT!: if parameterList != NULL
+	   //cannot put it on the leftmost
+
+	    p = p->rightSibling;
+        }
+    }
+
+    SymbolAttribute* symbol_att = (SymbolAttribute*) malloc(sizeof(SymbolAttribute));
+    symbol_att->attributeKind = FUNCTION_SIGNATURE;
+    symbol_att->attr = functionSig;
+    enterSymbol(function_name,symbol_att);
+}
+
+
---
+++ hw4/2014HW4/symbolTable.c
@@ -0,0 +1,145 @@
+#include "symbolTable.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+// This file is for reference only, you are not required to follow the implementation. //
+
+#define SCOPE_SIZE 1000
+
+int HASH(char * str) {
+	int idx=0;
+	while (*str){
+		idx = idx << 1;
+		idx+=*str;
+		str++;
+	}
+	return (idx & (HASH_TABLE_SIZE-1));
+}
+
+SymbolTable symbolTable;
+
+SymbolTableEntry* newSymbolTableEntry(int nestingLevel)
+{
+    SymbolTableEntry* symbolTableEntry = (SymbolTableEntry*)malloc(sizeof(SymbolTableEntry));
+    symbolTableEntry->nextInHashChain = NULL;
+    symbolTableEntry->prevInHashChain = NULL;
+    symbolTableEntry->nextInSameLevel = NULL;
+    symbolTableEntry->sameNameInOuterLevel = NULL;
+    symbolTableEntry->attribute = NULL;
+    symbolTableEntry->name = NULL;
+    symbolTableEntry->nestingLevel = nestingLevel;
+    return symbolTableEntry;
+}
+
+void removeFromHashTrain(int hashIndex, SymbolTableEntry* entry)
+{
+   if(entry->nextInHashChain != NULL){
+	entry->prevInHashChain->nextInHashChain = entry->nextInHashChain;
+	entry->nextInHashChain->prevInHashChain = entry->prevInHashChain;
+	
+	entry->nextInHashChain = NULL;
+	entry->prevInHashChain = NULL;
+	//free(entry);    
+   } 
+   else{
+	entry->prevInHashChain->nextInHashChain = NULL;
+	entry->prevInHashChain = NULL;
+   }
+}
+
+void enterIntoHashTrain(int hashIndex, SymbolTableEntry* entry)
+{
+	entry->nextInHashChain = symbolTable.hashTable[hashIndex];
+	symbolTable.hashTable[hashIndex] = entry;
+   
+}
+
+void initializeSymbolTable()
+{
+   symbolTable.scopeDisplay = (SymbolTableEntry**)malloc(sizeof(SymbolTableEntry*)*SCOPE_SIZE);
+   symbolTable.currentLevel = -1;
+   //FIXME
+   symbolTable.scopeDisplayElementCount = 0;
+}
+
+void symbolTableEnd()
+{
+   free(symbolTable.scopeDisplay);
+   //free()
+}
+
+SymbolTableEntry* retrieveSymbol(char* symbolName)
+{
+     SymbolTableEntry* p = symbolTable.hashTable[HASH(symbolName)];
+     while(p != NULL && (strcmp(p->name,symbolName) != 0)){ p = p->nextInHashChain; }	
+     return p;
+}
+
+//int check_old_symbol_depth(SymbolTableEntry* old_symbol){
+//	if(old_symbol)
+//}
+
+SymbolTableEntry* enterSymbol(char* symbolName, SymbolAttribute* attribute)
+{
+	SymbolTableEntry* entry = newSymbolTableEntry(symbolTable.currentLevel);
+	entry->name = symbolName;
+	entry->attribute = attribute;
+        SymbolTableEntry* old_symbol = retrieveSymbol(entry->name);
+        if((old_symbol != NULL) && (old_symbol->nestingLevel == entry->nestingLevel)){
+	   //raise error
+	   printf("ID %s redeclared.\n");
+	   removeFromHashTrain(HASH(old_symbol->name),old_symbol);
+ 	}
+	enterIntoHashTrain(HASH(entry->name),entry);
+	//scope pointer
+ 	entry->nextInSameLevel = symbolTable.scopeDisplay[entry->nestingLevel]; 
+	symbolTable.scopeDisplay[entry->nestingLevel] = entry;
+
+}
+
+//remove the symbol from the current scope
+void removeSymbol(char* symbolName)
+{
+        SymbolTableEntry* old_symbol = retrieveSymbol(symbolName);
+        if((old_symbol != NULL) && (old_symbol->nestingLevel == symbolTable.currentLevel)){
+	   removeFromHashTrain(HASH(old_symbol->name),old_symbol);
+	   free(old_symbol);
+ 	}
+	else{
+	   printf("ID %s not found.\n");
+	}
+		
+}
+
+int declaredLocally(char* symbolName)
+{
+        SymbolTableEntry* old_symbol = retrieveSymbol(symbolName);
+        if((old_symbol != NULL) && (old_symbol->nestingLevel == symbolTable.currentLevel)){
+	   return 1;
+	}else{
+	   return 0;
+ 	}
+}
+
+void openScope()
+{
+  int i = ++symbolTable.currentLevel;
+  symbolTable.scopeDisplay[i] = NULL;
+  //FIXME 
+  if(symbolTable.currentLevel > symbolTable.scopeDisplayElementCount)
+  symbolTable.scopeDisplayElementCount = symbolTable.currentLevel;
+}
+
+void closeScope()
+{
+   SymbolTableEntry* p = symbolTable.scopeDisplay[symbolTable.currentLevel];
+   while(p != NULL){ 
+      SymbolTableEntry* next_node  = p->nextInSameLevel;
+      SymbolTableEntry* prev_sym  = p->sameNameInOuterLevel;
+      //free(p);
+      removeSymbol(p->name);
+      p = next_node;
+      enterIntoHashTrain(HASH(prev_sym->name),prev_sym);
+   }
+   symbolTable.scopeDisplay[symbolTable.currentLevel--] = NULL;
+}
